3c9bedf46fb6cd13bec594cabeefc223a94cee28
Raúl Wolters <56522208+smups@users.noreply.github.com>
2023-05-29 12:13:12 +0200

    added FitsNumber trait to facilitate working with FITS' big endian encoding

--- src/intern/extensions/image.rs
+++ src/intern/extensions/image.rs
@@ -19,19 +19,21 @@
   licensee subject to Dutch law as per article 15 of the EUPL.
 */
 
-use std::{thread, sync::{Mutex, mpsc::sync_channel, Arc}, ops::DerefMut};
+use std::{thread, sync::{Mutex, mpsc::sync_channel, Arc}, ops::DerefMut, error::Error};
 
-use crate::{intern::{HduOptions, fits_consts::BLOCK_SIZE}, io::FitsReader, hdu::HduData};
+use num_traits::Num;
+
+use crate::{intern::{HduOptions, fits_consts::BLOCK_SIZE}, io::FitsReader, hdu::HduData, err::io_err::FitsReadErr};
 
 pub fn read_image_hdu(opts: &HduOptions, reader: &mut (impl FitsReader + Send)) -> HduData {
-  //(1) Caculate the size in bytes of the image
-  let byte_size = opts
-    .shape()
-    .into_iter()
-    .fold(opts.bitpix().abs() as usize / 8, |acc, bpx| acc + (*bpx as usize));
+  //(1a) Caculate the size in bytes of the image
+  let n_entries = opts.shape().into_iter().fold(0, |acc, bpx| acc + (*bpx as usize));
+  let byte_size = (opts.bitpix().abs() as usize / 8) * n_entries;
 
-  //(2) Calculate the number of *full* FITS blocks we have to read
+  //(1b) Calculate the number of *full* FITS blocks we have to read
   let full_block_size = byte_size / BLOCK_SIZE;
+
+  //(2) Allocate a vec (of the appropriate type) large enough to hold all data
 
   //Two buffers: buf1 is owned by the main thread, buf2 by the io thread
   let mut buf1 = Box::new([0u8; BLOCK_SIZE]);
@@ -69,3 +71,86 @@ pub fn read_image_hdu(opts: &HduOptions, reader: &mut (impl FitsReader + Send)) 
 
   todo!()
 }
+
+fn read_typed_vec<T: Num>(n_entries: usize, reader: &mut (impl FitsReader + Send)) -> Result<Vec<T>, FitsReadErr> {
+  //(1) Pre-allocate output vec
+  let mut out = Vec::<T>::with_capacity(n_entries);
+
+  //Calculate number of blocks that we have to read
+  let n_full_blocks = std::mem::size_of::<T>() * n_entries / BLOCK_SIZE;
+
+  /*
+  * Explanation of IO strategy
+  * There will be two threads: one responsible for reading header blocks, and 
+  * one responsible for turning the raw bytes into typed data.
+  * 
+  */
+  let mut local_buf = Ok(Box::new([0u8; BLOCK_SIZE]));
+  let shared_buf = Mutex::new(Ok(Box::new([0u8; BLOCK_SIZE])));
+  let (tx, rx) = sync_channel::<bool>(0);
+
+  //() Create a scope to manage the buffer lifetimes
+  thread::scope(|scope| -> Result<(), FitsReadErr> {
+    let shared_buf_ref = &shared_buf;
+    
+    //Set-up the IO thread
+    scope.spawn(move || {
+      let mut io_buf = Ok(Box::new([0u8; BLOCK_SIZE]));
+      while rx.recv().unwrap() {
+        //Fill local buffer
+        io_buf = reader
+          .read_blocks_into(io_buf.as_mut().unwrap().deref_mut())
+          .and_then(|_| Ok(io_buf.unwrap()));
+        //Swap local and shared buffer
+        std::mem::swap(shared_buf_ref.lock().unwrap().deref_mut(), &mut io_buf);
+      }
+    });
+
+    //Instruct the IO thread to prepare the first read
+    tx.send(true);
+
+    //We do the parsing on the main thread (order of these ops is very important!)
+    for _ in 0..n_full_blocks {
+      //(1) Swap the buffers (read the result from the IO thread)
+      std::mem::swap(shared_buf_ref.lock().unwrap().deref_mut(), &mut local_buf);
+
+      //(2) Tell the IO thread to continue
+      tx.send(true);
+
+      //(3) Parse the buffer
+      if let Err(err) = local_buf {
+        return Err(err)
+      } else if let Ok(ref buf) = local_buf {
+        for raw in buf.chunks_exact(std::mem::size_of::<T>()) {
+
+        }
+      }
+    }
+
+    Ok(())
+  })?;
+
+  todo!()
+}
+
+trait FitsNumber: Num {
+  fn fits_decode(raw: &[u8]) -> Self;
+  fn fits_encode(self, dest: &mut [u8]);
+}
+
+macro_rules! impl_fitsnumber {
+  ($($type:ty),*) => {$(
+    impl FitsNumber for $type {
+      #[inline]
+      fn fits_decode(raw: &[u8]) -> Self {
+        Self::from_be_bytes(raw.try_into().expect("incorrect slice length. This is a bug in rustronomy-fits"))
+      }
+    
+      #[inline]
+      fn fits_encode(self, dest: &mut [u8]) {
+        dest.copy_from_slice(&self.to_be_bytes())
+      }
+    }
+  )*};
+}
+impl_fitsnumber!(u8, i16, i32, i64, f32, f64);
